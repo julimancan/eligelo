c91515069bc5827421c04b84452e66db88613d25
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-12 18:27:39 -0500

    Initial layout of  the QuienesSomos page

--- pages/quienesSomos.jsx
+++ pages/quienesSomos.jsx
@@ -1,6 +1,8 @@
 import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
 import { getSiteSettings } from "../sanity/queries/siteSettings";
 import { getQuienesSomosContent } from "../sanity/queries/pages/quienesSomos";
+import Seo from "../components/layout/header/Seo";
+import styled from "styled-components";
 
 
 export const getStaticProps = async (ctx) => {
@@ -21,13 +23,21 @@ export const getStaticProps = async (ctx) => {
 const QuienesSomos = () => {
   const {data: quienesSomosContent} = useQuery(["quienesSomosContent"], getQuienesSomosContent)
 
+  const {SEO}= quienesSomosContent;
   console.log({quienesSomosContent});
   return (
-    <div>
-      Enter
-    </div>
+    <>
+      <Seo description={SEO.description} title={SEO.title}/>
+      <StyledQuienesSomos>
+        <h1 className="title">{quienesSomosContent.hero.title}</h1>
+      </StyledQuienesSomos>
+    </>
   );
 }
+
+const StyledQuienesSomos = styled.main`
+
+`;
 
 
 export default QuienesSomos;
