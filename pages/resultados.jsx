318a34196c3331bf94bf1c021fdcc2026bea42e0
Julian Bustos <julian.m.bustos@gmail.com>
2022-11-13 22:08:03 -0500

    ads react query to the results page to update the results

--- pages/resultados.jsx
+++ pages/resultados.jsx
@@ -1,77 +1,38 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { useState } from "react";
+import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
 import SearchBar from "../components/SearchBar";
-import {
-  getBikesByBrandName,
-  getAnyResultsFromText,
-  getMotosByBrandName,
-  getScootersByBrandName,
-} from "../sanity/queries/pages/resultados";
+import { getAnyResultsFromText } from "../sanity/queries/pages/resultados";
 
 export const getServerSideProps = async ({ query }) => {
   const { search } = query;
 
-  const searchResults = await getAnyResultsFromText(search);
-  // const motoResults = await getMotosByBrandName(search);
-  // const bikeResults = await getBikesByBrandName(search);
-  // const scooterResults = await getScootersByBrandName(search);
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(["searchResults"], () =>
+    getAnyResultsFromText(search)
+  );
 
   return {
     props: {
-      data: null,
-      searchResults,
-      // motoResults,
-      // bikeResults,
-      // scooterResults,
+      dehydratedState: dehydrate(queryClient),
     },
   };
 };
 
-const Resultados = ({
-  searchResults,
-}) => {
+const Resultados = ({}) => {
+  const { data: searchResults } = useQuery(["searchResults"], () =>
+    getAnyResultsFromText(search)
+  );
 
-  const router = useRouter();
-  const [cars, setCars] = useState(searchResults);
-  // const [motos, setMotos] = useState(motoResults);
-  // const [bikes, setBikes] = useState(bikeResults);
-  // const [scooters, setScooters] = useState(scooterResults);
-  useEffect(() => {
-    console.log("new search", router.query.search)
-  }, [router.query.search])
-  console.log({ searchResults });
   return (
     <main>
-      <SearchBar/>
-      {cars?.map((car, index) => (
+      <SearchBar />
+      {searchResults?.map((item, index) => (
         <div>
-          <h1>{car.brand?.name}</h1>
-          <h2>{car.model?.name}</h2>
-          <p>{car._type}</p>
+          <h1>{item.brand?.name}</h1>
+          <h2>{item.model?.name}</h2>
+          <p>{item._type}</p>
         </div>
       ))}
-      {/* {motos?.map((moto, index) => (
-        <div>
-          {moto.brand?.name}
-          {moto.model?.name}
-          {moto._type}
-        </div>
-      ))}
-      {bikes?.map((bike, index) => (
-        <div>
-          {bike.brand?.name}
-          {bike.model?.name}
-          {bike._type}
-        </div>
-      ))}
-      {scooters?.map((scooter, index) => (
-        <div>
-          {scooter.brand?.name}
-          {scooter.model?.name}
-          {scooter._type}
-        </div>
-      ))} */}
     </main>
   );
 };
