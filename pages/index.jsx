adddbf6b3ec95fb29d46da9978846849d3b2d75b
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-14 15:05:23 -0500

    Initial Layout to homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,8 @@ import Logo from "/public/logo.svg"
 import BlueCity from "/public/homepageElements/blue-city.svg"
 import BlueSedan from "/public/homepageElements/blue-sedan.svg"
 import { getHomepageContent } from "../sanity/queries/pages/homepage";
+import Seo from "../components/layout/header/Seo";
+import styled from "styled-components";
 
 export const getStaticProps = async () => {
   const queryClient = new QueryClient();
@@ -24,15 +26,48 @@ const Homepage = () => {
   // console.log({ siteSettings });
   const {data: pageContent} = useQuery(["homepapge"], getHomepageContent);
 
+  const {SEO} = pageContent;
   console.log({pageContent})
   return (
-    <main>
-      <Logo/>
-      <BlueCity/>
-      <BlueSedan/>
-      <SearchBar/>
-    </main>
+    <>
+      <Seo description={SEO.description} title={SEO.title} />
+      <StyledHomePage>
+        <Logo/>
+        <article className="hero">
+          <BlueCity/>
+          <BlueSedan/>
+          <section>
+            <h1>{pageContent.hero.blackText}<span>{pageContent.hero.blueText}</span></h1>
+          </section>
+        </article>
+
+        <SearchBar/>
+
+        <article className="products">
+          <h2>{pageContent.productsTitle}</h2>
+          <section className="product">
+            <h3>Carros</h3>
+          </section>
+
+          <section className="product">
+            <h3>Motos</h3>
+          </section>
+
+          <section className="product">
+            <h3>Bicicletas</h3>
+          </section>
+
+          <section className="product">
+            <h3>Patinetas</h3>
+          </section>
+        </article>
+      </StyledHomePage>
+    </>
   );
 };
+
+const StyledHomePage = styled.main`
+ padding: 0 !important;
+`
 
 export default Homepage;
