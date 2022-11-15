8b721fa9cdc45b5d7527336ddb2d31d61351e8b5
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-14 20:28:36 -0500

    add styles to products

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,14 @@ import BlueSedan from "/public/homepageElements/blue-sedan.svg";
 import { getHomepageContent } from "../sanity/queries/pages/homepage";
 import Seo from "../components/layout/header/Seo";
 import styled from "styled-components";
+import CarProductBack from "/public/homepageElements/car-product-background.svg";
+import CarProduct from "/public/homepageElements/car-product.svg";
+import MotoProductBack from "/public/homepageElements/moto-product-background.svg";
+import MotoProduct from "/public/homepageElements/moto-product.svg";
+import BiciProductBack from "/public/homepageElements/bici-product-background.svg";
+import BiciProduct from "/public/homepageElements/bici-product.svg";
+import PatinetaProductBack from "/public/homepageElements/patineta-product-background.svg";
+import PatinetaProduct from "/public/homepageElements/patineta-product.svg";
 
 export const getStaticProps = async () => {
   const queryClient = new QueryClient();
@@ -33,10 +41,10 @@ const Homepage = () => {
     <>
       <Seo description={SEO.description} title={SEO.title} />
       <StyledHomePage>
-        <Logo className="logo"/>
+        <Logo className="logo" />
         <article className="hero">
-          <BlueCity className="background"/>
-          <BlueSedan className="image"/>
+          <BlueCity className="background" />
+          <BlueSedan className="image" />
           <section>
             <h1 className="title">
               {pageContent.hero.blackText}
@@ -45,26 +53,38 @@ const Homepage = () => {
           </section>
         </article>
 
-        <SearchBar />
+        <section className="search">
+          <SearchBar />
+        </section>
 
-        <article className="products">
-          <h2>{pageContent.productsTitle}</h2>
-          <section className="product">
-            <h3>Carros</h3>
-          </section>
+        <section className="products-section">
+          <h2 className="title">{pageContent.productsTitle}</h2>
+          <section className="products">
+            <section className="product">
+              <h3>Carros</h3>
+              <CarProductBack className="back"/>
+              <CarProduct className="element"/>
+            </section>
 
-          <section className="product">
-            <h3>Motos</h3>
-          </section>
+            <section className="product">
+              <h3>Motos</h3>
+              <MotoProductBack className="back"/>
+              <MotoProduct className="element"/>
+            </section>
 
-          <section className="product">
-            <h3>Bicicletas</h3>
-          </section>
+            <section className="product">
+              <h3>Bicicletas</h3>
+              <BiciProductBack className="back"/>
+              <BiciProduct className="element"/>
+            </section>
 
-          <section className="product">
-            <h3>Patinetas</h3>
+            <section className="product">
+              <h3>Patinetas</h3>
+              <PatinetaProductBack className="back"/>
+              <PatinetaProduct className="element"/>
+            </section>
           </section>
-        </article>
+        </section>
       </StyledHomePage>
     </>
   );
@@ -73,14 +93,14 @@ const Homepage = () => {
 const StyledHomePage = styled.main`
   padding: 0 !important;
 
-  .logo{
+  .logo {
     margin: 0 auto;
   }
-  
+
   .hero {
     position: relative;
-    &::after{
-      content: '';
+    &::after {
+      content: "";
       display: block;
       height: 3.5rem;
       width: 100%;
@@ -93,13 +113,61 @@ const StyledHomePage = styled.main`
       width: 90%;
       text-align: center;
       transform: translateX(-50%);
-      span{
+      span {
         color: var(--primary-blue);
       }
     }
-    .image{
+    .image {
       position: absolute;
       bottom: -5%;
+    }
+  }
+
+  .search {
+    padding: 1rem;
+    background-color: var(--primary-blue);
+  }
+  .products-section{
+    padding: 1rem 1rem 4.3rem;
+    background-color: var(--primary-blue);
+    color: var(--light-gray);
+  
+    .title{
+      padding-bottom: 1.3rem;
+    }
+
+    .products {
+      display: grid;
+      display: grid;
+      gap: 1rem;
+      grid-auto-rows: 12.188rem;
+      grid-template-columns: repeat(auto-fit, minmax(10.25rem, 1fr));
+    }
+  }
+
+  .product{
+    background-color: var(--blue-2);
+    border-radius: 8px;
+    padding: 1rem 1rem .1rem;
+    position: relative;
+    display: flex;
+    overflow: hidden;
+    flex-direction: column;
+    justify-content: space-between;
+
+    h3{
+      z-index: 1;
+    }
+
+    .element{
+      position: sticky;
+    }
+    
+    .back{
+      height: 90%;
+      left: 0;
+      bottom: 1rem;
+      position: absolute;
     }
   }
 `;
