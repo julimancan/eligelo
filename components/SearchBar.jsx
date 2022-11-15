c5b767ccc1e74e4e478d346572854f72fd5a92fa
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-14 19:47:32 -0500

    add styles to SearchBar component

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,4 +1,8 @@
 import { useRouter } from "next/router";
+import BlueCarSearch from "/public/icons/blue-car-search.svg";
+import styled from "styled-components";
+import { Roboto } from "@next/font/google";
+const roboto = Roboto({ weight: "500" });
 
 const SearchBar = ({ resultsPage = false }) => {
   const router = useRouter();
@@ -15,7 +19,8 @@ const SearchBar = ({ resultsPage = false }) => {
     router.push({ pathname: `/resultados/`, query: { search }, shallow: true });
   };
   return (
-    <form onSubmit={handleSearch}>
+    <StyledSearchBar onSubmit={handleSearch} className={roboto.className}>
+      <BlueCarSearch class="icon" />
       {resultsPage ? (
         <input
           placeholder="¿Qué vehiculo estas buscando?"
@@ -30,8 +35,37 @@ const SearchBar = ({ resultsPage = false }) => {
           name="search"
         />
       )}
-    </form>
+    </StyledSearchBar>
   );
 };
+
+const StyledSearchBar = styled.form`
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+  .icon {
+    position: absolute;
+    left: 0.8rem;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  input {
+    width: 100%;
+    height: 2.5rem;
+    border: none;
+    padding: 12px 2.7rem;
+    border-radius: 8px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+
+    &:focus-visible {
+      outline: none;
+    }
+  }
+`;
 
 export default SearchBar;
