297ac2a733727206b4d7a5e5d6994eac5a440402
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-15 11:52:29 -0500

    Initial layout to catalogo

--- pages/catalogo.jsx
+++ pages/catalogo.jsx
@@ -4,6 +4,8 @@ import { getSiteSettings } from "../sanity/queries/siteSettings";
 import Logo from "/public/logo.svg"
 import {Inter} from "@next/font/google"
 import SearchBar from "../components/SearchBar";
+import styled from "styled-components";
+import Seo from "../components/layout/header/Seo";
 
 const inter = Inter()
 
@@ -24,14 +26,45 @@ export const getStaticProps = async (ctx) => {
 const Catalogo = () => {
   const {data: pageContent} = useQuery(["catalogo"], getCatalogoContent);
 
+  const {SEO} = pageContent
   console.log(pageContent)
   return (
-    <main className={inter.className}>
-      <Logo />
-      <h3>{pageContent.slogan.slogan}</h3>
-      <SearchBar />
-    </main>
+    <>
+      <Seo description={SEO.description} title={SEO.title} />
+      <StyledCatalogo className={inter.className}>
+        <header className="header">
+          <Logo className="logo"/>
+          <h3>{pageContent.slogan.slogan}</h3>
+        </header>
+
+        <section className="content">
+          <SearchBar />
+
+        </section>
+
+      </StyledCatalogo>
+    </>
   );
 }
+
+const StyledCatalogo = styled.main`
+  padding: 0 !important;
+
+  .header{
+    text-align: center;
+    margin-bottom: .7rem;
+    .logo{
+      margin: .2rem auto;
+    }
+  }
+
+  .content{
+    padding: 1.5rem 1rem;
+    background-color: var(--primary-blue);
+    min-height: 100vh;
+
+  }
+
+`
 
 export default Catalogo;
