def34f10897b4c2fe094ebcab4f19e6ef1e0d9ab
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-19 12:55:09 -0500

    add styles to desktop version from ContactLabels

--- components/layout/footer/ContactLabels.jsx
+++ components/layout/footer/ContactLabels.jsx
@@ -10,28 +10,31 @@ const ContactLabels = ({ horario, celular, email }) => {
       <section className="contact-method">
         <Whatsapp />
         <div>
-          <h4>Whatsapp</h4>
-          <p>
-            {celular} - Horario: {horario}
-          </p>
+          <h4 className="method-title">Whatsapp</h4>
+          <section className="text-info">
+            <p className="text">{celular}</p>
+            <p className="horario">Horario: {horario}</p>
+          </section>
         </div>
       </section>
       <section className="contact-method">
         <Celular />
         <div>
-          <h4>Teléfono</h4>
-          <p>
-            {celular} - Horario: {horario}
-          </p>
+          <h4 className="method-title">Teléfono</h4>
+          <section className="text-info">
+            <p className="text">{celular}</p>
+            <p className="horario">Horario: {horario}</p>
+          </section>
         </div>
       </section>
       <section className="contact-method">
         <Email />
         <div>
-          <h4>Correo</h4>
-          <p>
-            {email} - Horario: {horario}
-          </p>
+          <h4 className="method-title">Correo</h4>
+          <section className="text-info">
+            <p className="text">{email}</p>
+            <p className="horario"> Horario: {horario}</p>
+          </section>
         </div>
       </section>
     </StyledContactLabels>
@@ -39,19 +42,57 @@ const ContactLabels = ({ horario, celular, email }) => {
 };
 
 const StyledContactLabels = styled.section`
-    padding-top: 1.5rem;
-    padding-bottom: 1rem;
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
+  padding-top: 1.5rem;
+  padding-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 
-    .contact-method{
-        display: flex;
-        gap: 0.75rem;
-        svg{
-            margin-top: .25rem;
-        }
+  .contact-method {
+    display: flex;
+    gap: 0.75rem;
+    svg {
+      margin-top: 0.25rem;
+      min-width: min-content;
     }
+
+    .text-info {
+      display: flex;
+      .text::after {
+        content: " - ";
+      }
+      .horario {
+        margin-left: 0.3rem;
+      }
+    }
+  }
+
+  @media (min-width: 900px) {
+    flex-direction: row;
+    padding-top: 2.5rem;
+    .contact-method {
+      padding: 0 1rem;
+      border-left: 0.5px solid var(--light-gray);
+
+      .method-title {
+        font-size: 16px;
+        line-height: 16.94px;
+      }
+
+      .text-info {
+        flex-direction: column;
+        .text::after {
+          content: "";
+        }
+        .horario {
+          margin-left: 0;
+        }
+      }
+    }
+    .contact-method:first-child{
+        border: none;
+    }
+  }
 `;
 
 export default ContactLabels;
