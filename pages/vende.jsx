acd4a0c8df0ddb301e6b920fbdd20b92601b1a3b
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-30 20:09:13 -0500

    Initial layout

--- pages/vende.jsx
+++ pages/vende.jsx
@@ -1,28 +1,50 @@
 import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
+import styled from "styled-components";
+import Seo from "../components/layout/header/Seo";
 import { getVendeContent } from "../sanity/queries/pages/vende";
 import { getSiteSettings } from "../sanity/queries/siteSettings";
 
 export const getStaticProps = async () => {
-
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery(["siteSettings"], getSiteSettings)
-  await queryClient.prefetchQuery(["vendeContent"], getVendeContent)
+  await queryClient.prefetchQuery(["siteSettings"], getSiteSettings);
+  await queryClient.prefetchQuery(["vendeContent"], getVendeContent);
 
   return {
-    props:{
-      dehydratedState: dehydrate(queryClient)
-    }
-  }
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
 };
 const Vende = () => {
   // TODO the data for the contact information (phone #, horario, etc) is in the siteSettings
-  const {data: siteSettings} = useQuery(["siteSettings"], getSiteSettings)
+  const { data: siteSettings } = useQuery(["siteSettings"], getSiteSettings);
 
-  const {data: pageContent} = useQuery(["vendeContent"], getVendeContent)
+  const { data: pageContent } = useQuery(["vendeContent"], getVendeContent);
 
-  console.log({pageContent})
-  return <div>Enter</div>;
+  const { SEO } = pageContent;
+  console.log({ pageContent });
+  return (
+    <>
+      <Seo description={SEO.description} title={SEO.title}/>
+      <StyledVende>
+        <section className="hero">
+          <h1>{pageContent.hero.blackText} <span className="title">{pageContent.hero.blueText}</span></h1>
+        </section>
+      </StyledVende>
+    </>
+  );
 };
+
+const StyledVende = styled.main`
+  padding: 0 !important;
+
+  .hero{
+    span{
+      color: var(--primary-blue);
+    }
+  }
+
+`;
 
 export default Vende;
