344292b57484df0a19cf6fe1c6b70dd2e25d2ec1
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-12-17 20:05:03 -0500

    Implement rangeSlider to range component

--- components/Resultados/Range.tsx
+++ components/Resultados/Range.tsx
@@ -1,33 +1,42 @@
-import React from 'react'
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
+import RangeSlider from "./RangeSlider";
 
 type RangeProps = {
-    title: string,
-    min: string,
-    max: string,
-}
+  title: string;
+  min: string;
+  max: string;
+};
 
-const Range = ({title, min, max}:RangeProps) => {
+const Range = ({ title, min, max }: RangeProps) => {
+    const [value, setValue] = React.useState({ min: 5, max: 100 });
+
   return (
     <StyledRange>
-        <h3>{title}</h3>
-        {/* slider */}
-        <section className='values'>
-            <div className='min'>
-                <p>Mínimo</p>
-                <div>{min}</div>
-            </div>
-            <div className='max'>
-                <p>Máximo</p>
-                <div>{max}</div>
-            </div>
-        </section>
+      <h3>{title}</h3>
+      {/* slider */}
+      <RangeSlider
+        min={0}
+        max={100}
+        step={1}
+        value={value}
+        onChange={setValue}
+      />
+
+      <section className="values">
+        <div className="min">
+          <p>Mínimo</p>
+          <div>{min}</div>
+        </div>
+        <div className="max">
+          <p>Máximo</p>
+          <div>{max}</div>
+        </div>
+      </section>
     </StyledRange>
-  )
-}
+  );
+};
 
-const StyledRange = styled.section`
+const StyledRange = styled.section``;
 
-`
-
-export default Range
+export default Range;
