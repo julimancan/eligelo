1889b6e9405b38367ddc66b53de4267a12431f59
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-12-28 19:40:28 -0500

    Implement imageSlider to slug

--- pages/vehiculos/[slug].jsx
+++ pages/vehiculos/[slug].jsx
@@ -1,9 +1,8 @@
 import styled from "@emotion/styled";
 import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
-import Image from "next/image";
 import SearchBar from "../../components/SearchBar";
 import Button from "../../components/Button";
-import Details from "../../components/vehiclepage/Details"
+import Details from "../../components/vehiclepage/Details";
 import {
   getAllVehiclePathsAndNames,
   getAllVehicleSlugs,
@@ -12,8 +11,8 @@ import {
 import { getSiteSettings } from "../../sanity/queries/siteSettings";
 
 import { Inter } from "@next/font/google";
-import ProductCard from "../../components/ProductCard";
 import ProductSlider from "../../components/catalogo/ProductSlider";
+import ImageSlider from "../../components/vehiclepage/ImageSlider";
 
 const inter = Inter({ weight: "variable" });
 
@@ -64,27 +63,20 @@ const VehiclePage = ({ slug }) => {
       </section>
 
       <section className="content-grid">
-
-      {/* pictures slider */}
-        <section className="image-slider">
-          <picture className="image">
-            <Image
-              src={vehicleInfo.images[0].url}
-              width={vehicleInfo.images[0].width}
-              height={vehicleInfo.images[0].height}
-              alt={vehicleInfo.images[0].alt || 'hola'}
-            />
-          </picture>
+        {/* pictures slider */}
+        <section className="image-gallery">
+          <ImageSlider images={vehicleInfo.images} />
         </section>
 
         <section className="title-price">
-          <h1>{vehicleInfo.brand.name} - {vehicleInfo.model.name}</h1>
-          <h2>$ {vehicleInfo.price.toLocaleString("es-ES")} COP
-          </h2>
+          <h1>
+            {vehicleInfo.brand.name} - {vehicleInfo.model.name}
+          </h1>
+          <h2>$ {vehicleInfo.price.toLocaleString("es-ES")} COP</h2>
         </section>
 
         <section className="detalles">
-          <Details vehicleInfo={vehicleInfo}/>
+          <Details vehicleInfo={vehicleInfo} />
         </section>
 
         <section className="contact-section">
@@ -95,141 +87,140 @@ const VehiclePage = ({ slug }) => {
         </section>
 
         <section className="vehiculos-similares">
-
-            <ProductSlider className="vehiculos-slider" products={[vehicleInfo,vehicleInfo,vehicleInfo]} productName="Vehículos similares: "/>
-
+          <ProductSlider
+            className="vehiculos-slider"
+            products={[vehicleInfo, vehicleInfo, vehicleInfo]}
+            productName="Vehículos similares: "
+          />
         </section>
-
       </section>
-        
-      
     </StyledVehiclePage>
   );
 };
 
 const StyledVehiclePage = styled.main`
   padding: 0 !important;
-  
-  .search{ 
+
+  .search {
     background-color: var(--primary-blue);
     padding: 1rem;
-    form{
+    form {
       filter: none;
     }
   }
-  
-  .content-grid{
+
+  .content-grid {
     display: grid;
     grid-template-columns: repeat(1, 1fr);
 
-    .image-slider{
-      display: grid;
-      padding: 1rem;
-      place-content: center;
+    .image-gallery {
+      position: relative;
       background-color: var(--primary-blue);
+      padding: 1rem;
+      height: 55vw;
+      width: 100%;
+      aspect-ratio: calc(16 / 9);
     }
 
-    .title-price{
+    .title-price {
       background-color: var(--primary-blue);
       color: white;
       display: flex;
       flex-direction: column;
-      gap: .75rem;
+      gap: 0.75rem;
       padding: 1rem 1rem 3.5rem;
-      h1{
+      h1 {
         font-weight: 400;
       }
-      h2{
+      h2 {
         font-size: 24px;
         line-height: 19.5px;
       }
     }
 
-    .detalles{
+    .detalles {
       padding: 3.1rem 1rem 2.6rem;
     }
-  
-    .contact-section{
+
+    .contact-section {
       padding: 1rem;
       display: flex;
       flex-direction: column;
-      gap: .5rem;
-      .btn-whatsapp{
-        background-color: #25D366;
+      gap: 0.5rem;
+      .btn-whatsapp {
+        background-color: #25d366;
         width: 100%;
       }
     }
 
-      .vehiculos-similares{
-        padding: 1rem;
-        padding-right: 0;
-        .vehiculos-slider{
-          .header-product h3{
-            padding-bottom: 2rem;
-          }
+    .vehiculos-similares {
+      padding: 1rem;
+      padding-right: 0;
+      overflow-x: hidden;
+      .vehiculos-slider {
+        .header-product h3 {
+          padding-bottom: 2rem;
         }
       }
     }
+  }
 
-
-  @media (min-width: 900px){
-    .search{ 
+  @media (min-width: 900px) {
+    .search {
       padding: 2rem 7% 2.5rem;
     }
-    
-    .content-grid{
+
+    .content-grid {
       grid-template-columns: repeat(12, 1fr);
-      grid-template-rows: repeat(2,auto);
-      .image-slider{
+      grid-template-rows: repeat(2, auto);
+      .image-gallery {
         padding-bottom: 3.5rem;
         grid-column: 1 / 8;
         grid-row: span 2;
+        height: auto;
       }
-      .title-price{
+      .title-price {
         padding-right: 15%;
         grid-column: 8 / 13;
       }
-      .contact-section{
+      .contact-section {
         padding-right: 20%;
         background-color: var(--primary-blue);
         color: white;
         grid-column: 8 / 13;
         grid-row: 2 / 3;
       }
-      .detalles{
+      .detalles {
         padding: 5rem 15%;
         grid-column: 1 / 7;
       }
-      .vehiculos-similares{
+      .vehiculos-similares {
         padding-top: 5rem;
         grid-column: 7 / 13;
-        .vehiculos-slider{
+        .vehiculos-slider {
           display: grid;
-          .header-product{
+          .header-product {
             margin-bottom: 0;
-            h3{
+            h3 {
               font-size: 32px;
               line-height: 24px;
-              padding-bottom: .75rem;
+              padding-bottom: 0.75rem;
             }
-          } 
-          .list-products{
+          }
+          .list-products {
             width: auto;
             padding-left: 0;
             padding-right: 0;
             display: grid;
             grid-template-columns: min-content min-content;
-            li{
+            li {
               margin: 0 auto;
             }
-            
           }
         }
       }
     }
-
   }
-
-`
+`;
 
 export default VehiclePage;
