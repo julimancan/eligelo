e43eb5ae6fa7de366d1f7eff0f79d0ec469053b1
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-12-30 17:03:55 -0500

    Added feature to show more images

--- components/vehiclepage/ThumbnailImage.tsx
+++ components/vehiclepage/ThumbnailImage.tsx
@@ -1,45 +1,67 @@
 import styled from "@emotion/styled";
 import Image from "next/image";
-import React from "react";
-import useState from "react";
+import { useState } from "react";
 
 type ThumbnailImageProps = {
   images: {
-    alt?: string,
-    aspectRatio: number,
-    height: number,
-    url: string,
-    width: number,
-  }[],
-  index: number,
-  setIndex: React.Dispatch<React.SetStateAction<number>>,
-  className?: string,
+    alt?: string;
+    aspectRatio: number;
+    height: number;
+    url: string;
+    width: number;
+  }[];
+  className?: string;
 };
 
-const ThumbnailImage = ({ images, index, setIndex, className = "" }: ThumbnailImageProps) => {
-  const thumbnailElement = images.map((img, idx) => {
-    return (
-      <Image
-        key={img.url}
-        src={img.url}
-        alt={img.alt}
-        width={img.width}
-        height={img.height}
-        style={{ objectFit: "cover" }}
-        loading={"lazy"}
-        // alt="product"
-        onClick={() => setIndex(idx)}
-        className={idx === index ? "current" : ""}
-      />
-    );
-  });
+const ThumbnailImage = ({ images, className = "" }: ThumbnailImageProps) => {
+  const [index, setIndex] = useState(0);
+  const thumbnailElement = images
+    .filter((img, idx) => {
+      if (idx < 6) {
+        return img;
+      }
+    })
+    .map((img, idx) => {
+      if (idx === 5 && images.length > 6) {
+        return (
+          <section
+            key={`${img.url}-${idx}`}
+            className="more-images">
+            <Image
+              src={img.url}
+              alt={"vehicle"}
+              width={img.width}
+              height={img.height}
+              style={{ objectFit: "cover" }}
+              loading={"lazy"}
+              onClick={() => setIndex(idx)}
+              className={idx === index ? "current" : ""}
+            />
+            <p>+{images.length - 6}</p>
+          </section>
+        );
+      }
+      return (
+        <Image
+          key={`${img.url}-${idx}`}
+          src={img.url}
+          alt={"vehicle"}
+          width={img.width}
+          height={img.height}
+          style={{ objectFit: "cover" }}
+          loading={"lazy"}
+          onClick={() => setIndex(idx)}
+          className={idx === index ? "current" : ""}
+        />
+      );
+    });
   return (
     <StyledThumbnailImage className={className}>
       <div className="thumbnail-container">{thumbnailElement}</div>
       <div className="image-container">
         <Image
           src={images[index].url}
-          alt="product"
+          alt={"vehicle"}
           width={images[index].width}
           height={images[index].height}
           style={{ objectFit: "cover" }}
@@ -55,16 +77,15 @@ const StyledThumbnailImage = styled.section`
   border-radius: 5px;
   padding: 0.5rem;
   display: flex;
-  align-items: flex-start;
   gap: 0.6rem;
-  
+
   .image-container {
     display: flex;
     height: 504.25px;
     justify-content: flex-start;
     align-items: flex-start;
     width: 100%;
-    img{
+    img {
       width: 100%;
       height: 100%;
     }
@@ -72,19 +93,36 @@ const StyledThumbnailImage = styled.section`
 
   .thumbnail-container {
     display: flex;
+    height: auto;
+    gap: 1rem;
     flex-direction: column;
-    gap: 0.6rem;
-  }
-  .thumbnail-container img {
-    width: 100%;
-    height: 70px;
-    object-fit: cover;
-    cursor: pointer;
+
+    img {
+      width: 98.69px;
+      height: 74.02px;
+      cursor: pointer;
+    }
+
+    .more-images {
+      position: relative;
+      p {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        display: grid;
+        place-content: center;
+        color: white;
+        font-weight: 700;
+        font-size: 40px;
+        line-height: 39px;
+        background: rgba(31, 31, 31, 0.5);
+      }
+    }
   }
   .current {
     filter: grayscale(100%);
   }
-
 `;
 
 export default ThumbnailImage;
